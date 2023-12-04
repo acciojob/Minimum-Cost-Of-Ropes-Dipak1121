9f7611b01ba3306bc31e57344598d4ab700861da
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-12-04 16:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	let ans = document.getElementById("result");
+  let input = document.getElementById("rope-lengths");
+	let arr = input.value.split(",").map((num)=>{
+		return parseInt(num);
+	});
+	
+	let minCost = 0;
+	while(arr.length > 1){
+		arr.sort((a,b)=>{
+		return a - b;
+	});
+		let a = arr.shift();
+		let b = arr.shift();
+		minCost = a + b + minCost;
+		arr.push(a+b);
+	}
+
+	ans.innerText = minCost;
 }  
